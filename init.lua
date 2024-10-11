52579a9d57f17d6c841740c0becc7703b15b0aa6
Johnathan <johnathantostudy@gmail.com>
2024-10-11 12:15:06 -0300

    mais coisa

--- init.lua
+++ init.lua
@@ -2,9 +2,8 @@ require("config.lazy")
 require("config.conf")
 require("autoclose").setup()
 
-
 vim.cmd[[colorscheme catppuccin-mocha]]
-vim.cmd[[autocmd VimEnter * NERDTree]]
 vim.cmd[[autocmd BufWinEnter * if &buftype != 'quickfix']]
 vim.cmd[[nnoremap <F3> :NERDTreeToggle<CR>]]
 vim.cmd[[nnoremap <C-n> :NERDTreeFocus<CR>]]
+vim.cmd[[filetype indent on]]
