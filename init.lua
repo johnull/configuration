27355455ff36c28f01092b9d1e2f7a7c84e8ca62
Johnathan <johnathantostudy@gmail.com>
2024-10-11 09:12:51 -0300

    adicionando coisas

--- init.lua
+++ init.lua
@@ -4,4 +4,7 @@ require("autoclose").setup()
 
 
 vim.cmd[[colorscheme catppuccin-mocha]]
-
+vim.cmd[[autocmd VimEnter * NERDTree]]
+vim.cmd[[autocmd BufWinEnter * if &buftype != 'quickfix']]
+vim.cmd[[nnoremap <F3> :NERDTreeToggle<CR>]]
+vim.cmd[[nnoremap <C-n> :NERDTreeFocus<CR>]]
